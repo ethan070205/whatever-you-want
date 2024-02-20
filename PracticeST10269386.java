ff68bf10b897d1a64f13fbe301d2bb1575daf983
St10269386 <st10269386@vcconnect.edu.za>
2024-02-20 15:47:03 +0200

    im so cool

---
+++ PracticeST10269386.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package practicest10269386;
+
+/**
+ *
+ * @author User
+ */
+public class PracticeST10269386 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("this is the practice");
+        System.out.println("yes");
+        System.out.println("eoijfweiudjpjo");
+    }
+    
+}
